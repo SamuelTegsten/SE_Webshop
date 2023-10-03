7d4449306d456c857a39594a1a77065b0dc935df
Samuel Tegsten <samuel.tegsten@gmail.com>
2023-10-03 18:46:42 +0200

    Comments for the DbObjects

--- src/main/java/com/web/se_webshop/DB/BDObjects/DbItem.java
+++ src/main/java/com/web/se_webshop/DB/BDObjects/DbItem.java
@@ -6,16 +6,35 @@ import com.web.se_webshop.DB.DBManager.DBConnect;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * This class extends the Item class and provides functionality for executing CRUD operations on the database
+ */
 public class DbItem extends Item {
+
+    /**
+     * Constructor to create a DbItem object.
+     * @param name     The name of the item.
+     * @param picture  The picture URL of the item.
+     * @param category The category of the item.
+     * @param price    The price of the item.
+     */
     private DbItem(String name, String picture, String category, float price) {
         super(picture, name, category, price);
     }
 
-    public static void addItem(Item item, int stockNumber) throws SQLException {
+    /**
+     * Adds an item to the database with a specified stock number.
+     * @param item        The Item object to be added to the database.
+     * @param stockNumber The stock number of the item.
+     * @throws SQLException If a database error occurs during the operation.
+     */
 
+    public static void addItem(Item item, int stockNumber) throws SQLException {
+        // SQL statement for inserting a new item with stock number.
         String sql = "INSERT INTO Item VALUES(?, ?, ?, ?, ?)";
         PreparedStatement pstmt = null;
         try {
+            // Prepare the SQL statement and disable auto-commit to start a transaction.
             pstmt = DBConnect.getConnection().prepareStatement(sql);
             DBConnect.getConnection().setAutoCommit(false);
             pstmt.setString(1, item.getName());
@@ -24,14 +43,18 @@ public class DbItem extends Item {
             pstmt.setFloat(4, item.getPrice());
             pstmt.setInt(5, stockNumber);
 
+            // Execute the SQL statement to insert the item.
             pstmt.executeUpdate();
 
+            // Commit the transaction to save changes to the database.
             DBConnect.getConnection().commit();
 
         } catch (SQLException e) {
+            // Rollback the transaction and throw an exception if a database error occurs.
             DBConnect.getConnection().rollback();
             throw new SQLException(e);
         } finally {
+            // Close the prepared statement to release resources.
             if (pstmt != null) {
                 pstmt.close();
             }
--- src/main/java/com/web/se_webshop/DB/BDObjects/DbUser.java
+++ src/main/java/com/web/se_webshop/DB/BDObjects/DbUser.java
@@ -8,12 +8,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * This class extends the User class and provides functionality for CRUD operations on the database.
+ */
 public class DbUser extends User {
+
+    /**
+     * Constructor to create a DbUser object.
+     * @param username   The username of the user.
+     * @param password   The password of the user.
+     * @param permission The permission level of the user.
+     */
+
     protected DbUser(String username, String password, Permission permission) {
         super(username, password, permission);
     }
 
+    /**
+     * Adds a user to the database, checking for existing username or password before insertion.
+     * @param user The User object to be added to the database.
+     * @return True if the user was successfully added, false if the username or password already exists in the database.
+     * @throws SQLException If a database error occurs during the operation.
+     */
+
     public static boolean addUser(User user) throws SQLException {
+        // SQL statements for checking user existence and inserting a new user.
         String sqlCheckExistence = "SELECT * FROM User WHERE username = ? OR password = ?";
         String sqlInsertUser = "INSERT INTO User (username, password, permission) VALUES (?, ?, ?)";
 
@@ -21,15 +40,17 @@ public class DbUser extends User {
         PreparedStatement pstmtInsert = null;
 
         try {
+            // Check if the username or password already exists in the database.
             pstmtCheck = DBConnect.getConnection().prepareStatement(sqlCheckExistence);
             pstmtCheck.setString(1, user.getUsername());
             pstmtCheck.setString(2, user.getPassword());
             ResultSet resultSet = pstmtCheck.executeQuery();
 
             if (resultSet.next()) {
-                return false;
+                return false; // Username or password already exists.
             }
 
+            // Insert the new user into the database.
             pstmtInsert = DBConnect.getConnection().prepareStatement(sqlInsertUser);
             DBConnect.getConnection().setAutoCommit(false);
             pstmtInsert.setString(1, user.getUsername());
@@ -41,9 +62,11 @@ public class DbUser extends User {
             DBConnect.getConnection().commit();
 
         } catch (SQLException e) {
+            // Rollback transaction and throw an exception if a database error occurs.
             DBConnect.getConnection().rollback();
             throw new SQLException(e);
         } finally {
+            // Close prepared statements to release resources.
             if (pstmtCheck != null) {
                 pstmtCheck.close();
             }
@@ -51,7 +74,6 @@ public class DbUser extends User {
                 pstmtInsert.close();
             }
         }
-        return true;
+        return true; // User successfully added to the database.
     }
 }
-
